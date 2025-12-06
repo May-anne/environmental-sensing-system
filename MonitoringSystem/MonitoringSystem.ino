c53f99727acf5709e377efaa521868b4dbd1d2e5
Mayanne Gomes <mayane.viviane0511@gmail.com>
2025-12-06 16:12:48 -0300

    add lux monitoring

--- MonitoringSystem/MonitoringSystem.ino
+++ MonitoringSystem/MonitoringSystem.ino
@@ -13,6 +13,13 @@
 #define dhtPin 14
 #define soundPin 33
 
+const unsigned long intervalLUX = 2000;
+const unsigned long darkAlertTime = 120000;
+unsigned long lastLuxRead = 0;
+unsigned long darkStartTime = 0;
+bool darkPeriod = false; 
+bool darkLightDetected = false;
+
 const int sampleWindow = 200; // ms
 
 DHT dht11(dhtPin, DHT11);
@@ -37,16 +44,51 @@ void setup(){
 
   dht11.begin();
 
-  analogReadResolution(12);          // 0..4095
-  analogSetPinAttenuation(soundPin, ADC_11db); // tenta faixa maior
+  analogReadResolution(12);
+  analogSetPinAttenuation(soundPin, ADC_11db);
 }
 
 void loop() {
-}
+  unsigned long currentMillis = millis();
 
-float readLuxSensor(){
-  float lux = lightMeter.readLightLevel();
-  return lux;
+  /*-------- Bloco 1: Análise de Luminosidade --------*/
+  if (currentMillis - lastLuxRead >= intervalLUX) {
+    lastLuxRead = currentMillis;
+
+    float luxValue = lightMeter.readLightLevel();
+    Serial.print("Lux: ");
+    Serial.println(luxValue);
+
+    // ---------- Período claro ----------
+    if (!darkPeriod) {
+      if (luxValue < 150 || luxValue > 300) { //Aprimorar depois
+        Serial.println("Lux fora do ideal no período claro (150–300).");
+        // coloque ação aqui (LED, buzzer, etc.)
+      }
+      darkLightDetected = false;
+      darkStartTime = 0;
+    }
+
+    // ---------- Período escuro ----------
+    else {
+      if (luxValue > 0) {
+        if (!darkLightDetected) { //Se detectar luz, inicie contagem
+          darkStartTime = currentMillis;
+          darkLightDetected = true;
+        } else { //Luz ainda detectada detectada, meça o tempo
+          if (currentMillis - darkStartTime >= darkAlertTime) {
+            Serial.println("Luz detectada no período escuro por > 2 min.");
+            // coloque ação aqui
+          }
+        }
+      } else { //Lux não é zero (ou deixou de ser), resete flags
+        darkLightDetected = false;
+        darkStartTime = 0;
+      }
+    }
+  }
+
+  /*-------- Bloco 2: Análise de Umidade e Temperatura --------*/
 }
 
 void readDHT11Sensor(){
